6c073f6b6f354c267e377888a07a380d5aa59d15
aaca20 <aaca20@gmail.com>
2015-12-03 01:59:38 -0200

    Verificação dos e-mails e passwords se são iguais e do username se não é vazio concluídos

--- src/com/example/game/Cadastro.java
+++ src/com/example/game/Cadastro.java
@@ -2,7 +2,6 @@ package com.example.game;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,69 +16,101 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 @SuppressLint("NewApi") public class Cadastro extends ActionBarActivity {
-		
+
 	
-			//private Matcher matcher;
-		    
-			@Override
-		    protected void onCreate(Bundle savedInstanceState) {
-		        super.onCreate(savedInstanceState);
-		        setContentView(R.layout.activity_cadastro);
-		        
-		        final EditText email1 = (EditText) findViewById(R.id.email1);
-		        final EditText email2 = (EditText) findViewById(R.id.email2);
-		        final EditText password1 = (EditText)findViewById(R.id.password1);
-		        final EditText password2 = (EditText)findViewById(R.id.password2);
-		        EditText username = (EditText)findViewById(R.id.username);
-		        Button validar = (Button)findViewById(R.id.validate);
-		        
-		        validar.setOnClickListener(new OnClickListener() {
-					
-					@Override
-					public void onClick(View v) {
-						if (!validateEmail(email1.getText().toString())) {
-							email1.setError("Email Inválido");
-							email1.requestFocus();
-						}
-						else if (!validateEmail(email2.getText().toString())) {
-							password1.setError("Password Inválida");
-							email1.requestFocus();
-						}
-						else if (!validatePassword(password1.getText().toString())) {
-							password1.setError("Password Inválida");
-							email1.requestFocus();
-						} 
-						else if (!validatePassword(password2.getText().toString())) {
-							password1.setError("Password Inválida");
-							email1.requestFocus();
-						} 
-						else {
-							Toast.makeText(Cadastro.this, "Validação com sucesso", Toast.LENGTH_LONG).show();
-						}
-						
+		@Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	        
+	        final EditText email1 = (EditText) findViewById(R.id.email1);
+	        final EditText email2 = (EditText) findViewById(R.id.email2);
+	        final EditText password1 = (EditText)findViewById(R.id.password1);
+	        final EditText password2 = (EditText)findViewById(R.id.password2);
+	        final EditText username = (EditText)findViewById(R.id.username);
+	        Button validar = (Button)findViewById(R.id.validate);
+	        
+	        
+	        validar.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					if (!validateEmail(email1.getText().toString())) {
+						email1.setError("E-mail inválido");
+						email1.requestFocus();
 					}
-				});
-		        
-		    }
-		    // Retorna true se a senha é válida e false se a senha é inválida
-			protected boolean validatePassword(String password) {
-				if (password!=null && password.length()>=6) {
-					return true;
-				} else {
-					return false;	
+					else if (!validateEmail(email2.getText().toString())) {
+						email2.setError("E-mail inválido");
+						email2.requestFocus();
+					}
+					else if (!validateEmails(email1.getText().toString(), email2.getText().toString()) ) {
+						email2.setError("E-mails diferentes");
+						email2.requestFocus();
+					}
+					else if (!validateUsername(username.getText().toString())) {
+						username.setError("Username inválido");
+						username.requestFocus();
+					}
+					else if (!validatePassword(password1.getText().toString())) {
+						password1.setError("Password inválida");
+						password1.requestFocus();
+					} 
+					else if (!validatePassword(password2.getText().toString())) {
+						password2.setError("Password inválida");
+						password2.requestFocus();
+					}
+					else if (!validatePasswords(password1.getText().toString(), password2.getText().toString()) ) {
+						password2.setError("Passwords diferentes");
+						password2.requestFocus();
+					} 
+					else {
+						Toast.makeText(Cadastro.this, "Validação com sucesso", Toast.LENGTH_LONG).show();
+					}
+					
 				}
+			});
+	        
+	    }
+	     
+		// Retorna true se o email1 é válido e false se o email1 é inválido
+		protected boolean validateEmail(String email) {
+			String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 				
+			Pattern pattern = Pattern.compile(emailPattern);
+			Matcher matcher = pattern.matcher(email);
+			return matcher.matches();
+		}
+		
+		protected boolean validateEmails(String email1, String email2) {
+	    	if (email1.contains(email2)) {
+				return true;
+			} else {
+				return false;	
 			}
-
-			
-			// Retorna true se o email1 é válido e false se o email1 é inválido
-			protected boolean validateEmail(String email) {
-				String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-				
-				Pattern pattern = Pattern.compile(emailPattern);
-				Matcher matcher = pattern.matcher(email);
-				return matcher.matches();
+	    }
+		
+		protected boolean validateUsername(String username) {
+			if (username.length()!=0) {
+				return true;
+			} else {
+				return false;
 			}
+		}
 			
-
-}
+		// Retorna true se a senha é válida e false se a senha é inválida
+		protected boolean validatePassword(String password) {
+			if (password!=null && password.length()>=6) {
+				return true;
+			} else {
+				return false;	
+			}
+		}
+		
+		protected boolean validatePasswords(String password1, String password2) {
+			if (password1.contains(password2)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
